d2dd2f02ffd614777288ba183f6fed79a3b83906
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-01-30 10:55:10 +0600

    Added => while loop practice

---
+++ while.js
@@ -0,0 +1,10 @@
+/** *************************************** WHILE LOOP *************************************/
+// A while loop is used to repeat a specific block of code until a condition is met. A real life example would be sserving rice at a wedding repeatedly
+
+var roastGiven = 0;
+while (roastGiven < 7){
+    console.log('Roast Den, Please !!');
+    // roastGiven = roastGiven + 1;
+    // roastGiven += 1;
+    roastGiven++;
+}
